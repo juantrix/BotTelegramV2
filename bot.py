b398affb099912306f0d29d706e013dcc7394c7a
juan <juanostrit66@gmail.com>
2021-10-02 13:31:08 -0300

    Estructura de directorios agregada y abanze un poco en el codigo

---
+++ bot.py
@@ -0,0 +1,63 @@
+from os import system, listdir
+import time
+import telebot
+
+with open('datos.txt', 'r') as f:
+    datos = [line.strip() for line in f] #0: tiempo entre publicacion, 1: token del bot, 2: mensaje que sale debajo de la foto, 3: id del chat, 4: True si se quiere mantener las fotos enviadas(False borra)
+    f.close()
+
+bot = telebot.TeleBot(datos[1], parse_mode=None)
+
+videos = listdir('./videos')
+fotos = listdir('./fotos')
+audios = listdir('./audios')
+carpetas = [videos, fotos, audios]
+'''
+bot.send_video(datos[3], open('./videos/'+carpetas[0][0], 'rb'))
+bot.send_photo(datos[3], open('./fotos/'+carpetas[1][0], 'rb'))    
+bot.send_audio(datos[3], open('./audios/'+carpetas[2][0], 'rb'))
+
+'''
+
+while True:
+    videos = listdir('./videos')
+    fotos = listdir('./fotos')
+    audios = listdir('./audios')
+    carpetas = [videos, fotos, audios]
+
+    print(carpetas)
+
+    for carpeta in carpetas:
+        try:
+            bot.send_video(datos[3], open('./videos/'+carpeta[0], 'rb'))
+            if datos[4] == 'True':
+                system(f'mv videos/{carpeta[0]} videos_enviados/{carpeta[0]}')
+            else:
+                system(f'rm videos/{carpeta[0]}')
+
+            time.sleep(datos[0]*60)
+        except:
+            pass
+        try:
+            bot.send_photo(datos[3], open('./fotos/'+carpeta[0], 'rb'))    
+            if datos[4] == 'True':
+                system(f'mv fotos/{carpeta[0]} fotos_enviadas/{carpeta[0]}')
+            else:
+                system(f'rm fotos/{carpeta[0]}')   
+            time.sleep(datos[0]*60)  
+        except:
+            pass
+        try:
+            bot.send_audio(datos[3], open('./audios/'+carpeta[0], 'rb'))
+            if datos[4] == 'True':
+                system(f'mv audios/{carpeta[0]} audios_enviados/{carpeta[0]}')
+            else:
+                system(f'rm audios/{carpeta[0]}')
+            time.sleep(datos[0]*60)
+        except:
+            pass
+            
+    # mirar los contenidos de las carpetas
+
+    # enviar 1 archivo de una carpeta
+    # pasar a la siguiente carpeta
